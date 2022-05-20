070badb1f0bde655c8f4d71f733b73a329b88a07
aliaformo <103521035+aliaformo@users.noreply.github.com>
2022-05-21 01:30:58 +0200

    Add files via upload

---
+++ adding_integers_task.py
@@ -0,0 +1,25 @@
+
+print("Welcome to this Adding Integers program")
+
+adding_integers = True
+
+while adding_integers: 
+    first_number = input("Enter the first number: ")
+    second_number = input("Enter the second number: ")
+
+    try:
+        first_number = int(first_number)
+        second_number = int(second_number)
+        addition = first_number + second_number
+
+        print(f"First number {first_number} + Second number {second_number} is equal to {addition}")
+
+    except:
+        print("You should enter an integer number in each input")
+    
+    cont_decision = input("Would you like to add another pair of integers? [y,n]: ")
+    if cont_decision == "y":
+        pass
+    if cont_decision == "n":
+        adding_integers = False
+print("Thanks for using this Adding Integers program")
